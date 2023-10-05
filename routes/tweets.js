a78a76fbcdcff8e7730a7c066ebf889283b8764b
Ruben <rubenhamou@gmail.com>
2023-10-05 17:55:50 +0200

    add vercel json

--- routes/tweets.js
+++ routes/tweets.js
@@ -25,10 +25,9 @@ router.post('/new', function(req, res) {
 
 // Récupération des 5 derniers tweets (ie les 5 plus récents)
 router.get('/lasttweets', function(req, res) {
-//   Tweet.find().sort({date: -1}).limit(5).then(data => {
-//     res.json(data)
-//   })
-console.log('coucou from route');
+  Tweet.find().sort({date: -1}).limit(5).then(data => {
+    res.json(data)
+  })
 });
 
 // Incrémentation du nb de like d'un tweet
