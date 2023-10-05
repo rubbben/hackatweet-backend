9a7a5612c6eb0216ab8b112f3ae7ab7b06e6b2f2
laurent <lcappe@posteo.net>
2023-10-05 16:45:53 +0200

    routes 'tweets' like et delete écrites et testées dans Postman - ok

--- routes/tweets.js
+++ routes/tweets.js
@@ -3,7 +3,7 @@ var router = express.Router();
 let Tweet = require('../models/tweets');
 const { checkBody } = require('../modules/checkBody');
 
-/* GET users listing. */
+// Création d'un nouveau tweet
 router.post('/new', function(req, res) {
     if (!checkBody(req.body, ['text'])) {
         res.json({ result: false, error: 'Missing or empty fields' });
@@ -23,18 +23,23 @@ router.post('/new', function(req, res) {
 
 });
 
+// Récupération des 5 derniers tweets (ie les 5 plus récents)
 router.get('/lasttweets', function(req, res) {
   Tweet.find().sort({date: -1}).limit(5).then(data => {
     res.json(data)
   })
 });
 
+// Incrémentation du nb de like d'un tweet
 router.post('/like', function(req, res) {
-  
+  Tweet.updateOne({_id: req.body._id}, { $inc: { like: 1 }})
+       .then(data => res.json(data))
 });
 
+// Suppression d'un tweet
 router.delete('/delete', function(req, res) {
-  
+  Tweet.deleteOne({_id: req.body._id})
+       .then(data => res.json(data))  
 });
 
 module.exports = router;
