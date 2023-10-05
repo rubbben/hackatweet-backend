ef030ff1acf4b6e4dc8737767efef7be0a33859a
Ruben <rubenhamou@gmail.com>
2023-10-05 17:50:35 +0200

    add cors

--- app.js
+++ app.js
@@ -13,6 +13,9 @@ var hashtagsRouter = require('./routes/hashtags');
 
 var app = express();
 
+const cors = require('cors');
+app.use(cors());
+
 app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
