6cb3c85d4eff149ffc1ddee2cbfee0c21cf036c2
Ruben <rubenhamou@gmail.com>
2023-10-05 17:33:48 +0200

    routes hashtag

--- models/hashtags.js
+++ models/hashtags.js
@@ -2,7 +2,7 @@ const mongoose = require('mongoose');
 
 const hastagSchema = mongoose.Schema({
   text: String,
-  tweets: [{ type: mongoose.Schema.Types.ObjectId, ref: 'tweets' }]
+  tweetsIds: [{ type: mongoose.Schema.Types.ObjectId, ref: 'tweets' }]
 });
 
 const Hashtag = mongoose.model('hashtags', hastagSchema);
--- routes/hashtags.js
+++ routes/hashtags.js
@@ -1,10 +1,43 @@
 var express = require('express');
 var router = express.Router();
-// let Hashtag = require('../models/hashtags')
+let Hashtag = require('../models/hashtags')
 
-/* GET users listing. */
-router.get('/', function(req, res, next) {
-  res.send('HASHTAG');
+
+router.post('/add', function(req, res) {
+  
+  //Verification hashtag non existant
+  Hashtag.findOne({ text: req.body.text }).then(data => {
+
+    if (data === null) {
+      //Creation nouvel hashtag
+      let newHashtag = new Hashtag({
+        text: req.body.text,
+        tweetsIds: [req.body.tweetId],
+      });
+
+      newHashtag.save().then(data => {
+        res.json({ result: true, data })
+      });
+    } else {
+      //Hashtag deja existant dans la BDD
+      Hashtag.updateOne({text: req.body.text}, {$push: { tweetsIds: req.body.tweetId }}).then(data => {
+        res.json({result: true, data })
+      });
+    }
+
+  });
+
+});
+
+
+
+
+//Recupere tout les tweets du hashtag concerné
+router.get('/:namehashtag', function(req, res) {
+  console.log(req.params.namehashtag);
+  Hashtag.findOne({text: req.params.namehashtag}).populate('tweetsIds').then(data => {
+    res.json(data)
+  })
 });
 
 module.exports = router;
